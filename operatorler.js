b8f6098acdf12ca64ea00cd8d5d5f6a705724891
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-04-28 16:35:57 +0100

    13 Aritmetik ve Arttirma Azaltma Operatorleri

---
+++ operatorler.js
@@ -0,0 +1,23 @@
+// Aritmetik operatorler + - * / %
+let sayi1 = 10
+let sayi2 = 2
+
+let sonuc = sayi1 + sayi2;// 12
+sonuc =sayi1 - sayi2; //8 
+let sonuc1 = sayi1%sayi2 //modda kalani verir bize 10 un 2 ile bolumunden kalan sayidir. sayi ikiye tam bolunuyorsa cift bolunmuyorsa tektir 
+
+console.log(sonuc)
+console.log(sonuc1)
+
+let yas =27
+let metinOlarakYas ='27' // usttekiyle ayni degildir 
+
+let number1=20
+let number2=10
+//Arttirma ve azaltma operatoleri
+number1 = number1 +1;   //+1 21 
+number1++;  //+1 daha 22 //++ sag tarafta ol. icin once metin sonra arttirma olusur ama alttakiyle ayni anlamdadir
+++number1//+1 23usttekiyle ayni anlamdadir sol tarafinda ol. icin once attirma olusur 
+number1--;//-1 sagda old. icin once sayi gelir sonra arttirir
+--number1//-1 solda old. icin once arttirir sonra sayi gelir ustekiyle ayni islevi gorur tekfark consolda tek kullandigimizda fark edilir
+console.log(number1) 
