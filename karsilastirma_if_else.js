70eb176cb5de0d3db14a2f927e360ca0e0785494
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-04-29 18:17:02 +0100

    16 If Else ile Sart ifadeleri

---
+++ karsilastirma_if_else.js
@@ -0,0 +1,33 @@
+let benimYasim= 31 , onunYasi=31
+
+if (benimYasim<onunYasi){
+    console.log("ben senden buyugum");
+}else if(benimYasim === onunYasi){
+    console.log("yasimi zesit");
+}
+else{
+    console.log("sen benden buyuksun ");
+}
+
+let ismim = "muhammet";
+
+if(ismim === "muhammet")    //tek satir oldugunda curly braces kullanmamiza gerek yok 
+console.log("adim muhammet")
+
+let sayi1 = 10, sayi2=20;
+
+if(sayi1 <sayi2){
+    if(sayi1>20)
+    console.log("sayi1 hem sayi ikiden hem sayi birden buyuktur");
+    else
+    console.log("tam tersi");
+    
+}
+
+let asaySayiMi = true;
+if(asaySayiMi == true)
+{
+
+}
+if(asaySayiMi) //yaninda true yazmamiza gerek yok zaten true dur
+    
