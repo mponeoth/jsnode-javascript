0c16226073b0f5afffacae86d23cc6446db3a81f
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-05-02 14:59:44 +0100

    20 while dongusu ve break Continue komutlari
    while i ekledim sadece

--- console_alert_prompt.js
+++
@@ -1,9 +0,0 @@
-let myAge = 27
-console.log(myAge) 
-console.error('hata')
-console.warn('daha uzun yazilsaydi iyiydi')
-console.clear()//clear consoledaki bilgileri siler 
-console.table({ad:"muhammet",yas:27})//tablo olusturur
-alert("merhaba dunya")
-let girilenSayi = prompt('sayi giriniz') //bir veri girmesini ister aldik o veriyi bir degiskene atadik
-console.log(girilenSayi)
