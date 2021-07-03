0f38abe96ee9c867dbf65d2d582534f2e99b3da4
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-07-03 16:48:27 +0100

    77 ES6 ile rehber uygulamasi | Kisilerin kaydedilmesi ve okunmasi bolum 2

--- app2.js
+++ app2.js
@@ -14,21 +14,77 @@ class Kisi{
 //const fatma = new Kisi('fatma','esref','fatma@esref')  
 //emre.ad diyerek degerlerimize ulasabiliriz
 
+class Util{
+        static bosAlanBirakma(...alanlar){
+        let sonuc = true
+        alanlar.forEach(alan =>{
+            if(alan.length === 0){
+                sonuc = false
+                return false
+            }
+        })
+        return sonuc 
+    }
+}
+
 class Ekran{
     constructor(){  //kisiler de iki veyahut 3 4 5 bize kalmis sayisi her bir kisi olusturdgumuzda yeni kutu olusur cunku oraya biz emrenin hasanin fatmanin parametrelerini giricez fakat Ekranda ayni sey soz konusu degil bu yuzden constructor i bos biraktik
         this.ad = document.querySelector('#ad')
         this.soyad = document.querySelector('#soyad')
         this.email = document.querySelector('#email')
         this.ekleGuncelleButon = document.querySelector('.kaydetGuncelle')   
+        this.form = document.querySelector('#form-rehber').addEventListener('submit',
+        this.kaydetGuncelle.bind(this)) //suanki this ekran objemizi temsil etmesi icin bindladik
+        this.kisiListesi = document.querySelector('.kisi-listesi')
         this.depo = new Depo()      
+        this.kisileriEkranaYazdir()  
       }
-}
 
+    kisileriEkranaYazdir(){
+        this.depo.tumKisiler.forEach(kisiA =>{ //depomuzun icindeki tumkisilere Erisip elemanlar
+            this.kisiyiEkranaEkle(kisiA)
+        })
+    }
+    
+
+    kisiyiEkranaEkle(kisi){
+        const olusturulanTR = document.createElement('tr')
+        olusturulanTR.innerHTML = `<td>${kisi.ad}</td>
+        <td>${kisi.soyad}</td>
+        <td>${kisi.email}</td>
+        <td>
+        
+            <button class="btn btn--edit"><i class="far fa-edit"></i></button>
+            <button class="btn btn--delete"><i class="far fa-trash-alt"></i></button>
+           
+        </td>`
+        this.kisiListesi.appendChild(olusturulanTR)
+    }
+
+
+    kaydetGuncelle(e){
+    e.preventDefault();
+    const kisi = new Kisi(this.ad.value,this.soyad.value,this.email.value)
+    console.log(kisi);
+    const outcome = Util.bosAlanBirakma(kisi.ad,kisi.soyad,kisi.email)
+    //tum alanlar doldurulmus
+    if(outcome){
+        //yeni kisiyi ekrana ekler
+        this.kisiyiEkranaEkle(kisi) //buradaki kisi kaydetGuncelle icerisindeki kisi degiskenindendir
+        
+        //localStorage a ekle
+        this.depo.kisiEkle(kisi)
+    }
+    
+    else{ //bazi alanlar eksik
+        console.log('bos birakilmaz');
+    }
+}}
  
 class Depo{  //Uygulama ilk acildiginda veriler getirilir burada herhangi bir islem yapmak istedigimizde mesala API gibi ekran veya Kisinin haberi dahil olmaz
   
      constructor(){
-         this.tumKisiler = []; //burasi deponun gectigi heryerde gecerli cunku depo sinifimizin bir propertisi oldu  
+         this.tumKisiler =  this.kisileriGetir();  //kisileriGetir calistiginda tumkisilerin ici dolar 
      }
      kisileriGetir(){
         let tumKisilerLocal; //buradaki tumKisilerLocal buradaki fonksiyon icin gecerli
@@ -38,7 +94,6 @@ class Depo{  //Uygulama ilk acildiginda veriler getirilir burada herhangi bir is
         else{
             tumKisilerLocal = JSON.parse(localStorage.getItem('tumKisiler'))
         }
-        this.tumKisiler = tumKisilerLocal
 
         return tumKisilerLocal
      }
@@ -50,4 +105,8 @@ class Depo{  //Uygulama ilk acildiginda veriler getirilir burada herhangi bir is
 
 }
 
- 
+ document.addEventListener('DOMContentLoaded',function(e){ //butun HTML yapisi eklendikten sonra burada bir function calistiricaz
+     const ekran = new Ekran() //bunu yaptigimizda Ekrandaki constructor tetiklenir 
+ })
+
+
