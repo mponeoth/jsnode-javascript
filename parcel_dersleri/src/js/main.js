d15b1382483433b0cb61ea0752638aeeceee0a25
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-12-05 14:48:20 +0000

    115 parcel nedir
      <!-- NOT ONEMLI : burada source kodumuzun icinde normal js yerine aslinda typeScript dosyasida ekleyebilirdim
            PARCE isimli yapi browser in anlayamayacagi scss gibi typeScript gibi yapilari aslinda arka planda browser in anlayacagi
            yapiya donusturur parcel_dersleri/dist  dist klasorunun icine olusturucak yapilan islem aslinda budur -->

---
+++ parcel_dersleri/src/js/main.js
@@ -0,0 +1,6 @@
+console.log('main js calisti n mii hihi ')
+
+import ogrenci from "./ogrenci.js"
+
+const emre = new ogrenci("emre")
+emre.adiniSoyle()
---
+++ parcel_dersleri/src/js/ogrenci.js
@@ -0,0 +1,9 @@
+export default class ogrenci{
+    constructor(ad){
+        this.ad = ad
+    }
+
+    adiniSoyle(){
+        console.log(`benim adim ${this.ad}`)
+    }
+}
