be7883f7ede5d7d8cec0a21fb504395bff3edd34
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-12-13 00:59:05 +0000

    120 cache mantigi webpack HTML ve Clean pluginleri
    bu dersimizde cachlerimizi eskilerini silip yenilerini ekrana gonderdik
    //
     HtmlWebpackPlugin kullandik
    CleanWebpackPlugin kullandik

--- webpack_dersleri/src/index.js
+++ webpack_dersleri/src/index.js
@@ -2,8 +2,11 @@
 //css dosyasi olur sass dosyasi olur typescript olur bu ve bunun gibi yapilar icin farkli loader kullaniyoruz
 import Ogrenci from "./Ogreci"
 import './sass/mysass.scss'
+console.log("merhaba2")
 
 const ogrenci = new Ogrenci("emre")
 ogrenci.ogrenciBilgisi()
+console.log("merhaba2 nasilsin")
 
+console.log("merhaba")
 console.log("merhaba")
--- webpack_dersleri/webpack.config.js
+++ webpack_dersleri/webpack.config.js
@@ -3,17 +3,27 @@
 // mesela benim pc c12/muhamnmettir baska pc ise c14/emre dir herkesin pc si farklidir bu gibi durumlari bizim dinamik olarak cozmemiz gerekiyor. o yuzden require olarak almamiz gerekiyor
 //path i kullanarak bunu bulabiliriz 
 //mode un bircok tanimli olani var bunlardan development, production gibi biz buna development dedigimiz zaman hala gelisim asamasinda old. soyluyoruz ve bizim kodlarimiz hangi satirda olduklarini bize gosteriyor 
+//CleanWebpackPlugin bizim olusan main2 2asssdsdsd main2 sdsdsdsd  eski dosyarimizi supurmek icin kullaniriz 
+//catch kavrami bir web sitesine gittigimizde orn emrealtunbilek.com a gidiyoruz ilk acilis genellikle sitelerde yavas olur sonrakilere gore network kismina gidip js ve css kisimlarina bakmak istiyorum dosyalar getiriliyor yanlarinda size`a 
+//baktigimizda diyor ben bu dosyayi catchden cektim ben bu dosyayi (memorycatch) den cektim diye yazi goruruz  ben burada control + f5 bu cacth mekanizmasini iptal etmis oluyoruz ve ve dosyalarimizi catchden degil serverdan cekmis oluyoruz 
 
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path')
 
 module.exports = {
     entry:'./src/index.js', //ilk once nerden calismaya baslamasi gerektigini soyluyoruz
     output:{//amacimiz ise bir output olusturmak
-        filename:'main2.js', //surada olusturularacak yerin tam yolunu veriyor 
+        filename:'main2.[contenthash].js', //surada olusturularacak yerin tam yolunu veriyor 
         path: path.resolve(__dirname,'dist') 
     },
     mode:'development',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+        template:"./src/index.html"
+    })],
     module: {
         rules: [
             {
