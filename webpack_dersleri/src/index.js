1685959460f3137ed95f5836e80f45571026a02c
muhammetkuruoglu <muhammetkuruglu.123@gmail.com>
2021-12-18 11:46:23 +0000

    122 webpack html ve file loader kullanimlari
    biz burada webpack ve file dosyalarini resimlerimizi webpackde gostermek icin kullanmaliyiz html loader bize resimlerimize ekranimizda gostermemize olanak saglar  file loader ise bizim resimlerimizin png ,svg gibi yapilarini dosyamizda kabul goruilmesini saglar  index js , index html  webpack common js webpack.development.js webpack.producrion.js yazdigimiz dosyalar

--- webpack_dersleri/src/index.js
+++ webpack_dersleri/src/index.js
@@ -10,9 +10,17 @@ console.log("merhaba2 nasilsin")
 
 console.log("merhaba")
 console.log("merhaba")
+console.log("merhaba")
+console.log("merhaba")
+console.log("merhaba2")
+console.log("merhaba2")
+console.log("merhaba2")
 console.log("merhaba2")
 console.log("merhaba3")
 console.log("merhaba4")
-
+console.log("merhaba4")
+console.log("merhaba4")
+console.log("merhaba5")
+ 
 
 
